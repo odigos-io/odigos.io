837ad0e1675bf68a0543c17c4da661a7c1de1b6b
Eden Federman <eden@keyval.dev>
2023-01-16 17:29:47 -0800

    astro landing page

---
+++ astro-landing-page/src/config.mjs
@@ -0,0 +1,24 @@
+export const BLOG = {
+  disabled: false,
+  postsPerPage: 4,
+
+  blog: {
+    disabled: false,
+    pathname: "blog", // blog main path, you can change this to "articles" (/articles)
+  },
+
+  post: {
+    disabled: false,
+    pathname: "", // empty for /some-post, value for /pathname/some-post
+  },
+
+  category: {
+    disabled: false,
+    pathname: "category", // set empty to change from /category/some-category to /some-category
+  },
+
+  tag: {
+    disabled: false,
+    pathname: "tag", // set empty to change from /tag/some-tag to /some-tag
+  },
+};
